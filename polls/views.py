e92a6bcdae31835a3368297acaed25ea175ff419
habibullo <azimovhabibullo1@gmail.com>
2023-09-22 23:53:04 +0500

    permissions comit

--- polls/views.py
+++ polls/views.py
@@ -2,46 +2,28 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import NewsSerializer
 from .models import NewsModel
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-
-# Create your views here.
-
-class CreateAPiView(APIView):
-    def post(self, request, *args, **kwargs):
-        if str(request.user) != 'AnonymousUser':
-            if request.user.roles == 2:
-                serializer = NewsSerializer(data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors)
-            else:
-                return Response({'msg': 'only staff members can add'})
-        else:
-            return Response({'msg': 'only staff members can add'})
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .permissions import StaffPermissionClass, AdminPermissionClass
 
 
-class ListAPiView(APIView):
-    def get(self, request, *args, **kwargs):
-        if str(request.user) == 'AnonymousUser':
-            return Response({'msg': 'Please log in'})
-        all = NewsModel.objects.filter(status=True)
-        serializer = NewsSerializer(all, many=True)
-        return Response(serializer.data)
+
+class CreateView(generics.CreateAPIView):
+    queryset = NewsModel.objects.all()
+    serializer_class = NewsSerializer
+    permission_classes = (IsAuthenticated, StaffPermissionClass)
 
 
-class UpdateStatus(APIView):
-    def patch(self, request, *args, **kwargs):
-        if str(request.user) != 'AnonymousUser':
-            if request.user.roles == 3:
-                news = get_object_or_404(NewsModel, id=kwargs['news_id'])
-                serializer = NewsSerializer(news, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors)
-            else:
-                return Response({'msg': 'only admins change status'})
+class AllApiView(generics.ListAPIView):
+    serializer_class = NewsSerializer
+    permission_classes = (IsAuthenticated,)
 
-        return Response({'msg': 'only admins change status'})
+    def get_queryset(self):
+        return NewsModel.objects.filter(status=True)
+
+
+class UpdateStatus(generics.UpdateAPIView):
+    queryset = NewsModel.objects.all()
+    serializer_class = NewsSerializer
+    permission_classes = (IsAuthenticated, AdminPermissionClass)
